db182f153cdd3382febc6d334966669294045cf6
M-Hasan-Ibrahim <107994287+M-Hasan-Ibrahim@users.noreply.github.com>
2023-03-18 18:44:40 +0200

    added full project

---
+++ Quiz/LOLPlayer/punishment.js
@@ -0,0 +1,7 @@
+const name = prompt("Enter Your Name");
+
+alert(
+  "Welcome " +
+    name +
+    " to your punishment\nGood luck trying to find the right crack button\nEnjoy"
+);
---
+++ Quiz/quiz.js
@@ -0,0 +1,10 @@
+// alert("hello");
+
+// const checker = document.getElementById("lolPlayer");
+
+function checker() {
+  var result = confirm("Are you sure about that?");
+  if (result === false) {
+    event.preventDefault();
+  }
+}
