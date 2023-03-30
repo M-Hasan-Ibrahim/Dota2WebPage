86a16f5651d7ed6e485d95f81f3693f0fe29d8cd
M-Hasan-Ibrahim <107994287+M-Hasan-Ibrahim@users.noreply.github.com>
2023-03-30 11:01:08 +0300

    punishment page fix

--- Quiz/LOLPlayer/punishment.js
+++ Quiz/LOLPlayer/punishment.js
@@ -3,5 +3,18 @@ const name = prompt("Enter Your Name");
 alert(
   "Welcome " +
     name +
-    " to your punishment\nGood luck trying to find the right crack button\nEnjoy"
+    " to your punishment\nGood luck trying to find the right crack button\nNote that the crack button is random\nEnjoy"
 );
+
+function Crack() {
+  choice = Math.floor(Math.random() * 7);
+  if (choice == 6) {
+    alert(
+      "Congratulations you passed the test\nYou may go back to the disabled page and retake the test"
+    );
+
+    location.href = "https://m-hasan-ibrahim.github.io/Dota2WebPage";
+  } else {
+    alert("not smart enough, try again");
+  }
+}
